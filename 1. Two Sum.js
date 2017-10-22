21aba4085a5caa6ece6db6a7fbf13d85d57cc10e
Anuj Pant <anuj.pant41@gmail.com>
2017-10-22 12:17:46 -0400

    working

--- 1. Two Sum.js
+++ 1. Two Sum.js
@@ -1,8 +1,9 @@
 var twoSum = function(nums, target) {
     for (var i = 0; i < nums.length; i++) {
       let toFind = target-nums[i];
-      if (nums.indexOf(toFind) >= 0) {
-        return [i, nums.indexOf(toFind)];
+      if (nums.slice(i+1).indexOf(toFind) >= 0) {
+        let index = nums.slice(i+1).indexOf(toFind)+1+i;
+        return [i, index];
       }
     }
   };
